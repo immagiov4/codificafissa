cb903606ed5caecf76ddc12ddb66146472fde12e
giovannibrancaccio <55757849+giovannibrancaccio@users.noreply.github.com>
2019-09-25 10:02:46 +0200

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -1,101 +1,110 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
-public class main {
-	static List<Persona> personsDatabase = new ArrayList<Persona>();
+public class MainProgram 
+{
+	
+	static Map<String, String> dizionarioInCodice = new HashMap<String, String>();
+	static String stringInput = "";
+	static Scanner keyboard = new Scanner(System.in);
+	static List<String> significati = new ArrayList<String>();
+	static List<String> significanti = new ArrayList<String>();
+	static List<String> listaTemporanea = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		while (true)
-			Commands();
-
+		
+		CreateDictionaries();
+		ConvertText();
 	}
 
-	static void print(String text) {
-		System.out.println(text);
-	}
-
-	static void Commands() {
-		Scanner keyboard = new Scanner(System.in); // System.in è l'input stream di default
-		int intInput = 0;
-		String stringInput = "";
-		do {
-			print("[1] Aggiungi persona [2] Cerca persona [3] Elimina utente: ");
-			intInput = keyboard.nextInt();
-		} while (intInput < 0 && intInput > 3);
-
-		if (intInput == 1) { // AGGIUNGI PERSONA
-			Persona person = Persona.CreatePerson();
-			boolean personExists = false;
-
-			for (int i = 0; i < personsDatabase.size(); i++) {
-				// Controlla se la persona esiste già
-				if (personsDatabase.get(i).ComparePersons(person))
-					personExists = true;
-			}
-
-			// Aggiunge la persona al database, se non è già presente
-			if (!personExists)
-				personsDatabase.add(person);
-			else
-				print("Questa persona è già nel database");
-
-		} else if (intInput == 2) { // CERCA PERSONA
-			do {
-				print("[1] Ricerca rapida [2] Ricerca normale: ");
-				intInput = keyboard.nextInt();
-			} while (intInput < 0 && intInput > 2);
-
-			if (intInput == 2) {
-				Persona person = Persona.CreatePerson();
-				boolean personExists = false;
-
-				for (int i = 0; i < personsDatabase.size(); i++) {
-					if (personsDatabase.get(i).ComparePersons(person))
-						personExists = true;
-				}
-
-				// Conferma la presenza della persona nel database
-				if (personExists)
-					print(person.nome + " " + person.cognome + " è presente nel database");
-				else
-					print(person.nome + " " + person.cognome + " non è presente nel database");
-
-			} else if (intInput == 1) {
-				String nome = "";
-				String cognome = "";
-
-				stringInput = keyboard.nextLine();
-
-				// Prende in input nome e cognome della persona da cercare e poi li compara a
-				// quelli nella lista
-				print("Nome: ");
-				stringInput = keyboard.nextLine();
-				nome = stringInput.toLowerCase();
-
-				print("Cognome: ");
-				stringInput = keyboard.nextLine();
-				cognome = stringInput.toLowerCase();
-
-				for (int i = 0; i < personsDatabase.size(); i++) {
-					if (personsDatabase.get(i).nome.contains(nome) && personsDatabase.get(i).cognome.contains(cognome))
-						personsDatabase.get(i).PrintData();
-					else if (i == personsDatabase.size())
-						print(nome + " " + cognome + " non è nel database");
-				}
-			}
-
-		} else if (intInput == 3) { // ELIMINA PERSONA
-
-			Persona person = Persona.CreatePerson();
-
-			for (int i = 0; i < personsDatabase.size(); i++) {
-				if (personsDatabase.get(i).ComparePersons(person)) {
-					personsDatabase.remove(i);
-					print("Utente rimosso dal database.");
-				} else
-					print(person.nome + " " + person.cognome + " non è nel database");
-			}
+	
+    static void print(String text) {
+        System.out.println(text);
+    }
+    
+    static void ReplaceListWithList(List<String> list1, List<String> list2)
+    {
+		list1.clear();
+    	for (int i4 = 0; i4 < list2.size(); i4++) {
+			list1.add(list2.get(i4));
 		}
-	}
+    }
+
+    static void CreateDictionaries()
+    {
+    	
+		boolean getInput = true;
+
+
+		int lunghezzaSignificante = 1;
+		
+		do {
+            print("Aggiungi un significato nell'ordine di assegnazione (digitare @E per uscire):\n ");
+            stringInput = keyboard.nextLine();
+            if (stringInput.contains("@E")) getInput = false;
+            else significati.add(stringInput);
+        } while (getInput);
+		
+		getInput = true;
+		
+		do {
+            print("Aggiungi un significante nell'ordine di assegnazione (digitare @E per uscire):\n ");
+            stringInput = keyboard.nextLine();
+            if (stringInput.contains("@E")) getInput = false;
+            else significanti.add(stringInput);
+        } while (getInput);
+		
+        ReplaceListWithList(listaTemporanea, significanti);
+        
+		while ((Math.pow(significanti.size(),lunghezzaSignificante)) < significati.size())
+		{
+			lunghezzaSignificante++;
+		}
+		
+		List<String> listaTemporanea2 = new ArrayList<String>();
+		
+		while (listaTemporanea.get(0).length() < lunghezzaSignificante)
+		{
+						
+			for (int i2 = 0; i2 < listaTemporanea.size(); i2++) {
+				for (int i3 = 0; i3 < significanti.size(); i3++) {
+
+					listaTemporanea2.add((listaTemporanea.get(i2) + significanti.get(i3)));
+				}
+			}
+			
+		
+			listaTemporanea.clear();
+
+			ReplaceListWithList(listaTemporanea, listaTemporanea2);
+			
+			listaTemporanea2.clear();
+		}
+		
+		//FUORI WHILE
+
+		ReplaceListWithList(significanti, listaTemporanea);
+		for (int i = 0; i < significati.size(); i++)
+		{
+			System.out.println(significanti.get(i) + " = " + significati.get(i));
+			dizionarioInCodice.put(significati.get(i), significanti.get(i));
+		}
+		System.out.println(dizionarioInCodice);
+    }
+
+    static void ConvertText()
+    {
+    	System.out.println("Inserisci il testo da convertire: ");
+    	
+    	stringInput = keyboard.nextLine();
+    	
+    	for (int i = 0; i < significati.size(); i++) {
+    	stringInput = stringInput.replaceAll(significati.get(i).toString(), significanti.get(i).toString());
+    	}
+    	
+    	System.out.println(stringInput);
+    }
 }
